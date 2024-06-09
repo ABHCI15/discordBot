758ceb7b33cd1ca6cd7d78e1cf78cd7e410c96a2
ABHCI15 <113409577+ABHCI15@users.noreply.github.com>
2024-06-09 22:34:55 +0000

    oops forgot to commit hack hour

--- app.py
+++ app.py
@@ -9,138 +9,108 @@ from profanity import profanity
 import google.generativeai as palm
 import pprint
 from dotenv import load_dotenv
-
+from langchain_core.tools import Tool
+from langchain_community.document_loaders import UnstructuredFileLoader
+from langchain_google_genai import (
+    ChatGoogleGenerativeAI,
+    HarmBlockThreshold,
+    HarmCategory,
+    GoogleGenerativeAIEmbeddings,
+)
+from pprint import pprint
+import sqlalchemy as sa
+from langchain_community.utilities import SQLDatabase
+from langchain_community.document_loaders import PyMuPDFLoader
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_chroma import Chroma
+from langchain.chains import RetrievalQAWithSourcesChain
+# from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_experimental.text_splitter import SemanticChunker
 load_dotenv()
-
-keyAPI = str(os.environ['palm'])
-palm.configure(api_key=keyAPI)
-models = [m for m in palm.list_models() if 'generateText' in m.supported_generation_methods]
-# model = models[0].name
-model = palm.GenerativeModel('gemini-pro')
-
-# this is for running bot 24/7
-intents = discord.Intents.default()
-intents.members=True
-Token = os.environ['Token']
-prefix = "-"
-intents = discord.Intents.default()
+from langchain_core.tools import ToolException
+from langchain_core.tools import StructuredTool
+from langchain_community.agent_toolkits import SQLDatabaseToolkit
+from pathlib import Path
 
 
-client = discord.Client(intents=intents)
-bot = commands.Bot(command_prefix=prefix, case_insensitive=True)
+book_list = os.listdir("chembooks")
+embeddings = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004")
+text_splitter = SemanticChunker(embeddings=embeddings,breakpoint_threshold_type="standard_deviation")
+split = []
+for book in book_list:
+    file_name = Path(os.path.join("chembooks", book))
+    loader = PyMuPDFLoader(file_name,extract_images=True)
+    documents = loader.load()
+    split.append(text_splitter.split_documents(documents))
+global db
+db = Chroma.from_documents(documents=split, embedding=embeddings, persist_directory="./chem_chroma_db")
+        
+    
+        
+# def store_book(bookpath : str):
+#     loader = PyMuPDFLoader(bookpath,extract_images=True)
+#     documents = loader.load()
+#     embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+#     text_splitter = SemanticChunker(embeddings=embeddings,breakpoint_threshold_type="standard_deviation")
+#     split = text_splitter.split_documents(documents)
+#     global db
+#     db = Chroma.from_documents(documents=split, embedding=embeddings, persist_directory="./chem_chroma_db")   
+    
 
-depressing_words = ('I feel bored', 'i feel bored', 'my life sucks','I feel lonely', "?quote")
+async def chem_book_retriever(query: str):
+    try:
+        results = db._asimilarity_search_with_relevance_scores(query, 5)
+        return results
+    except:
+        raise ToolException("An error occurred while retrieving the chemistry textbook pages.")
 
-greeting_words = [
-    "Hey", "Greetings from the creators of Mr. Authority", "hello!", 'wassup'
-]
+chem_ret = StructuredTool.from_function(
+    func=chem_book_retriever,
+    input_type=str,
+    name="chemistry textbook retriever",
+    description="Retrieves the most relevant chemistry textbook pages for a given query, always use with any chemistry related concepts.",
+    handle_tool_error=True,
+)
 
-words_from_user = ('hi', 'wassup', 'hey', 'hello')
+# venv/lib/python3.11/site-packages/mendeleev/elements.db
+# db.as_retriever()
+# db.delete_collection
 
-default_bad_words = profanity.get_words()
+# async def get_docs(query: str):
+#     results = db._asimilarity_search_with_relevance_scores(query, 5)
 
-reply_to_bad_word = ('no', 'no bad words', "SHUT UP NO BAD WORDS", "I WARN YOU NOT TO USE BAD WORDS")
-
-
-
-def get_quote():
-    response = requests.get("https://zenquotes.io/api/random")
-    json_data = json.loads(response.text)
-    quote = json_data[0]['q'] + " -" + json_data[0]['a']
-    return (quote)
-
+db_chem = SQLDatabase.from_uri("sqlite:///elements.db")
+token = str(os.getenv("TOKEN"))
+bot = discord.Bot()
+llm = ChatGoogleGenerativeAI(model="models/gemini-1.5-pro", temperature=0.1, google_api_key=str(os.getenv("GEMINI")), safety_settings={ HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE, HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE, HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE})
+toolkit = SQLDatabaseToolkit(db=db_chem, llm=llm)
+context = toolkit.get_context()
+tools = [chem_ret]
+tools.append(toolkit.get_tools())
 
 @bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print(f"{bot.user} is ready and online!")
+    # store_chembooks()
+
+@bot.slash_command(name="hello", description="Say hello to the bot")
+async def hello(ctx: discord.ApplicationContext):
+    await ctx.respond("Hey!")
+    
+@bot.event
+async def on_member_join(member):
+    await member.send(
+        f'Welcome to the server, {member.mention}! Enjoy your stay here.')
+
+@bot.slash_command(name="ping", description="Check the bot's latency")
+async def ping(ctx: discord.ApplicationContext):
+    await ctx.respond(f"Pong! {round(bot.latency * 1000)}ms")
 
 @bot.command()
-@has_permissions(kick_members=True)
-async def kick(message, kick_member:discord.Member,*,reason=None):
-  if kick_member == bot.user:
-    print('u cannot kick me:angry:')
-  elif kick_member.top_role >= message.author.top_role:
-    print('no possible')
-  elif kick_member == message.author:
-    print('u cannot kick urself')
-  else:
-    await kick_member.kick(reason=reason)
-    await message.channel.send(f"user{kick_member} has been kicked")
-  
-
-
-
-#defining a message
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    msg = message.content
-    mention = message.author.mention
-    member = discord.Member
-	
-    #greets
-    if message.content.startswith(words_from_user):
-        await message.channel.send(random.choice(greeting_words))
-    #sends motivaional quotes
-    if message.content.startswith(depressing_words):
-        await message.channel.send(f'Here is a quote to cheer you up: ' + get_quote())
-      
-    #removes words
-    if message.content.startswith("?bad word remove "):
-        splited = msg.split()
-        sub_splitted = splited[3]
-        default_bad_words.remove(sub_splitted)
-        await message.channel.send('The word was removed')
-
-    #Add more words
-    if message.content.startswith("?bad word add "):
-        splited = msg.split()
-        sub_splitted = splited[3]
-        default_bad_words.append(sub_splitted)
-        await message.channel.send("The word was added")
-
-    if message.content.startswith("?chat "):
-        completion = palm.generate_text(model=model, prompt=message.content[6:], temperature=0.5)
-        await message.channel.send( f"{mention} {completion.result}" )
-
-    if message.content.startswith("?chatembed"):
-        completion = palm.generate_content(message.content[10:])
-			# completion = palm.generate_content(model=model, prompt=message.content[10:], temperature=0.5)
-        embed_discordchat=discord.Embed(title=f"Response to:{message.content[10:]}", description=f"'{mention}' \n{completion.result}")
-
-			#         embed_discordchat=discord.Embed(title=f"Response to:{message.content[10:]}", description=f"'{mention}' \n{completion.result}")
-        embed_discordchat.set_thumbnail(url="https://media.discordapp.net/attachments/1068024665949360188/1184324621584060486/colourful-google-logo-in-dark-background-free-vector.png?ex=658b8f41&is=65791a41&hm=ba74f4466098cc6aa6958084a5e9ad3e5662354d3db8733dc5b73c10a9247458&=&format=webp&quality=lossless&width=918&height=918")
-        await message.channel.send(embed=embed_discordchat)
-			
-			
-			
-			
-	
-			
+async def chatOrgo(ctx, message: discord.Option(discord.SlashCommandOptionType.string)):
+    pass
     
-    #deleting messages that contain toxicity
-    if any(word in msg for word in default_bad_words):
-        await message.delete()
-        await message.channel.send(f"{mention} {random.choice(reply_to_bad_word)}")
-        embed_discordBad=discord.Embed(title="Registered offence for mentioning a bad word", description=f"{mention} has spoken of a bad word, please contact admin for more information")
-        avatar_profile = message.author.avatar
-        embed_discordBad.set_thumbnail(url=avatar_profile)
-        await message.channel.send(embed=embed_discordBad)
-
-
-#welcoming the user to the server
-async def on_message_join(member):
-    channel = client.get_channel(876805975624020069)
-    embed=discord.Embed(title=f"Welcome {member.name}", description=f"Thanks for joining {member.guild.name}!") # F-Strings!
-    embed.set_thumbnail(url=member.avatar) # Set the embed's thumbnail to the member's avatar image!
-    
-    await channel.send(embed=embed)
 
 
 
-
-client.run(Token)
-
-# now it shud work with default bad words_from_user
+bot.run(token)
